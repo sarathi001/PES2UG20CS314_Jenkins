776676848201106eeba34cee806b17cda5a40755
M G Sarathi <mgsarathi@Ms-MacBook-Pro.local>
2023-02-17 08:44:30 +0530

    made changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout << "I have made changes to the file"
   return 0;
 }
