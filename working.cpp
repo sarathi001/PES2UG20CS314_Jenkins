b29a6a88637293f940cb672157c842e4668c68c6
M G Sarathi <mgsarathi@Ms-MacBook-Pro.local>
2023-02-17 10:34:11 +0530

    adding working.cpp file

---
+++ working.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "This is the new working file\n";
+  cout << "For pipeline\n";
+  return 0;
+}
